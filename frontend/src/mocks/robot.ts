76e7829072adad57f40f2eff0d100a0f5db95906
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2021-05-07 16:00:46 +0700

    chore: 🤡 Add robot mocks.

---
+++ frontend/src/mocks/robot.ts
@@ -0,0 +1,51 @@
+import { Robot } from '../types'
+
+const mockRobot: Robot[] = [
+  {
+    name: 'Alexandrine Aufderhar',
+    image: 'https://robohash.org/Alexandrine Aufderhar.png?size=120x120',
+    price: 354.17,
+    stock: 4,
+    createdAt: '3/28/2021',
+    material: 'plastic',
+    id: '2c3d6c29-2a63-4d7c-a86c-0fd324fa7cf2',
+  },
+  {
+    name: 'Matt Crist',
+    image: 'https://robohash.org/Matt Crist.png?size=120x120',
+    price: 221.45,
+    stock: 5,
+    createdAt: '11/5/2020',
+    material: 'cotton',
+    id: 'fc0e35a9-84d8-47da-acda-1383d50e437f',
+  },
+  {
+    name: 'Emmanuelle Hermiston',
+    image: 'https://robohash.org/Emmanuelle Hermiston.png?size=120x120',
+    price: 532.3,
+    stock: 10,
+    createdAt: '6/26/2020',
+    material: 'soft',
+    id: '9bdbef97-b0f8-4930-aa09-995f2c631973',
+  },
+  {
+    name: 'Jarvis Kovacek',
+    image: 'https://robohash.org/Jarvis Kovacek.png?size=120x120',
+    price: 156.61,
+    stock: 4,
+    createdAt: '7/10/2020',
+    material: 'rubber',
+    id: '91023822-cc52-4ce9-8da6-2875fd5258e0',
+  },
+  {
+    name: 'Bulah Swaniawski',
+    image: 'https://robohash.org/Bulah Swaniawski.png?size=120x120',
+    price: 5.59,
+    stock: 7,
+    createdAt: '8/28/2020',
+    material: 'metal',
+    id: 'd709ef38-0bfe-4a43-9594-87b429206f4e',
+  },
+]
+
+export default mockRobot
